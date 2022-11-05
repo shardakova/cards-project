e45cac8324d2b68ad133b14cface23fc86303d74
Olga Shardakova <olga.brazhkina@gmail.com>
2022-11-05 16:06:05 +0700

    добавить редактирование полей формы

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,30 @@
-const buttonEditElem = document.querySelector('.button_type_edit');
 const popupElem = document.querySelector('.popup');
+const buttonEditElem = document.querySelector('.button_type_edit');
 const buttonCloseElem = popupElem.querySelector('.button_type_close');
+let formElement = document.querySelector('.popup__content');
+let nameInput = formElement.querySelector('.form__item_el_name');
+let jobInput = formElement.querySelector('.form__item_el_activity');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__activity');
 
-const onOpen = (popup) => {
-  popup.classList.add('popup_opened');
-};
+function openPopup () {
+  popupElem.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
 
-const onClose = (popup) => {
-  popup.classList.remove('popup_opened');
-};
+function closePopup () {
+  popupElem.classList.remove('popup_opened');
+}
 
-buttonEditElem.addEventListener('click', () => {
-  onOpen(popupElem);
-});
+function formSubmitHandler (evt) {
+  evt.preventDefault();
 
-buttonCloseElem.addEventListener('click', () => {
-  onClose(popupElem);
-});
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closePopup ()
+}
+
+buttonEditElem.addEventListener ('click', openPopup);
+buttonCloseElem.addEventListener ('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
